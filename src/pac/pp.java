450404cef28369be5bdab535be239ae4c25bff96
Saurabh1499 <90036775+SaurabhMulay999@users.noreply.github.com>
2022-07-25 16:17:18 +0530

    success

---
+++ src/pac/pp.java
@@ -0,0 +1,15 @@
+package pac;
+import java.util.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class pp {
+	public static void main(String[] args)throws InterruptedException 
+	{
+		System.setProperty("webdriver.chrome.driver","/home/saurabh/Downloads/chromedriver");
+		WebDriver d=new ChromeDriver();
+		d.get("https://www.facebook.com");
+	}
+
+}
